105317bc1c3a7d7a39e914e5d4f534f56b585d29
Nimbcarlos <consultor.nimb@hotmail.com>
2023-10-11 11:07:11 -0300

    css pagina e post

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def page(request):
+    return render(
+        request,
+        'blog/pages/page.html'
+    )
+
+def post(request):
+    return render(
+        request,
+        'blog/pages/post.html'
+    )
+
 def index(request):
     return render(
         request,
